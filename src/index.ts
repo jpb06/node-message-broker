582a23774ede153378c1febac4ecef7854fa72b6
jpb <jpb.06@outlook.com>
2019-06-17 19:56:20 +0200

    Exports

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+﻿import { MessageBrokerConfig as Configuration } from './configuration/message.broker.configuration';
+import * as Queuing from './logic/queuing';
+
+export { Configuration, Queuing };
